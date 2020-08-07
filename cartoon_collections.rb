3a4baa17b3c10f2f645cdfc4c1f406a61a94b9c9
daniel campo <campo_daniel94@yahoo.com>
2020-08-07 14:46:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,7 @@ end
   return captain_calls
 end
 
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
