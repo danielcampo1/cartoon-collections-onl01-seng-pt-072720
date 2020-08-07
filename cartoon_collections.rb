f7bf4139d3fbd8cd27fb9b80ef5f3fbecce77eb6
daniel campo <campo_daniel94@yahoo.com>
2020-08-07 16:20:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,10 +24,12 @@ def find_the_cheese(cheddar_cheese)
  # 1. list cheeese 
  cheese_types = ["cheddar", "gouda", "camembert"]
  # 2.find the any cheese's from cheddar_cheese
- cheddar_cheese.find
+ cheddar_cheese.find do |che|
  # 3. return cheese
  
  puts 
 end
-
-cheese_types = ["cheddar", "gouda", "camembert"]
+# # sharks = ["Hammerhead", "Great White", "Tiger", "Whale"]
+# result = sharks.find {|item| item.include?("a")}
+# print result
+# cheese_types = ["cheddar", "gouda", "camembert"]
